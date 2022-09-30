58106575a5eb7d74d47343e73c3218c7bceb6268
sebazdev <szuviria@gmail.com>
2022-09-29 21:06:39 -0300

    Clase de promesas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React, { useState } from 'react'
 import './App.css';
-import Counter from './components/Counter/Counter';
-import Effect from './components/Effect/Effect';
+import Counter from './components/Counter/Counter'
+import Navbar from './components/Navbar/Navbar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
 
 function App() {
-  const [show, setShow] = useState(true)
-
-  const handleOnAdd = () => {
-    console.log('se hizo click en agregar al carrito')
-  }
-
-  const handleOnAdd2 = () => {
-    console.log('se hizo click en agregar al carrito 2')
-  }
-
   return (
     <div className="App">
-        {/* <Navbar />
-        <ItemListContainer greeting={'Hola'}/> */}
-        <Counter onAdd={handleOnAdd}/>
-        <Counter onAdd={handleOnAdd2}/>
-        {/* <button onClick={() => setShow(!show)}>show/hide</button>
-        {show ? <Effect /> : null} */}
+      <Navbar />
+      <ItemListContainer />
+      <Counter />
     </div>
   );
 }
